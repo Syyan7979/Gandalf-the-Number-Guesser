b47cd638f379319d7b6a72973a6b8e59754210eb
Syyan7979 <54343052+Syyan7979@users.noreply.github.com>
2021-04-21 19:07:49 +0800

    number wizard commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max, min;
+    [SerializeField] Text guessText;
+    [SerializeField] TextMeshProUGUI counterText;
+
+    int guess;
+    static int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        if (min < max + 1)
+        {
+            guess = Random.Range(min, max + 1);
+            counter += 1;
+            guessText.text = guess.ToString();
+        }
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+        
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+}
+
---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour
+{
+    
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void LoadPreviusScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
